8f1d66afcc607aac978c29859bfa0aa59373b8f0
Shahnawaz Sazid <shahnawazsazid60@gmail.com>
2025-02-03 23:38:15 +0600

    Layout Bug Fix

--- src/layouts/container/MainContainer.jsx
+++ src/layouts/container/MainContainer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const MainContainer = ({ children }) => {
     return (
-        <div className="container mx-auto xl:max-w-6xl 2xl:max-w-[1600px]">
+        <div className="container mx-auto xl:max-w-6xl 2xl:max-w-[1500px]">
             {children}
         </div>
     );
